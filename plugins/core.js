f4eaa603fefab0fe61c3d1f99c8b8917962f6285
Alexander Ivanov <47699394+Saiv46@users.noreply.github.com>
2021-09-21 00:13:30 +0800

    Add REQID handler on core

--- plugins/core.js
+++ plugins/core.js
@@ -1,4 +1,8 @@
 import nPlugin from "../src/plugin.js";
+import {
+	LEGACY_MINVERSION,
+	LEGACY_SERVERVERSION
+} from "../src/client.js";
 
 export default class GDTMP extends nPlugin {
 	static get id() { return "gdtmp_core" }
@@ -7,6 +11,27 @@ export default class GDTMP extends nPlugin {
 		if (client.data.op) {
 			client.sendLegacy("OPPED", true);
 		}
+		client.on("REQID", () => {
+			const { gameplay } = this.server.config
+			client.sendLegacy("SETTINGS", [
+				LEGACY_SERVERVERSION,
+				gameplay.timesync,
+				gameplay.syncconsoles,
+				gameplay.offlineconsoles,
+				gameplay.reviewbattle,
+				gameplay.serversidesave
+			]);
+		});
+		client.on("POLL", () => {
+			client.sendLegacy("POLLRES", [
+				this.clients.size - 1,
+				gameplay.cheatmodallowed,
+				LEGACY_MINVERSION,
+				gameplay.description,
+				LEGACY_SERVERVERSION
+			]);
+			client.kick("poll");
+		});
 	}
 	onClientJoined(client) {
 		client.on("MSG", ([msg]) => {
